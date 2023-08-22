f069181f9278ac2571d8b746803e6f80a36e93a3
kartik <kartikshrikhande11@gmail.com>
2023-08-22 12:38:00 +0530

    fixed bugs and update some changes

---
+++ index.js
@@ -0,0 +1,23 @@
+const express = require('express');
+const bodyParser = require('body-parser');
+const { default: mongoose } = require('mongoose');
+const route = require('./src/router/route');
+require('dotenv').config({path:".env"})
+
+const app = express();
+
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: true }));
+
+mongoose.set('strictQuery', true)
+mongoose.connect(process.env.MongoDB)  
+
+.then( () => console.log("MongoDb is Ready for you"))
+.catch ( err => console.log(err));
+
+app.use('/', route);
+
+
+app.listen(process.env.PORT, function () {
+    console.log('Express app running on port ' + process.env.PORT)
+});
